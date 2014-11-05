05b7f6214f07998d7aa03d8b34d2e0294f2efced
Perttu Ahola <celeron55@gmail.com>
2014-11-05 17:51:44 +0200

    get_current_clickable_draw_target(): Prioritize exact rectangle

--- h1edpi.js
+++ h1edpi.js
@@ -714,15 +714,25 @@ var CDT_PAD = 4
 h1e.get_current_clickable_draw_target = function(){
 	var mx = h1e.mouse.x
 	var my = h1e.mouse.y
+	var imprecise_target = undefined
 	for(var i=h1e.clickable_draw_targets.length-1; i>=0; i--){
 		var target = h1e.clickable_draw_targets[i]
+		if(target.cb === "__hide")
+			continue
 		var rect = target.rect
-		if(mx >= rect[0] - CDT_PAD && mx < rect[0] + rect[2] + CDT_PAD &&
-				my >= rect[1] - CDT_PAD && my < rect[1] + rect[3] + CDT_PAD){
+		// Prioritize exact rectangle
+		if(mx >= rect[0] && mx < rect[0] + rect[2] &&
+				my >= rect[1] && my < rect[1] + rect[3]){
 			return target
 		}
+		// Fall back to padded rectangle
+		if(mx >= rect[0] - CDT_PAD && mx < rect[0] + rect[2] + CDT_PAD &&
+				my >= rect[1] - CDT_PAD && my < rect[1] + rect[3] + CDT_PAD){
+			imprecise_target = target
+		}
 	}
-	return undefined
+	console.log("imprecise_target:", imprecise_target)
+	return imprecise_target
 }
 
 h1e.draw_current_clickable_draw_target = function(){
